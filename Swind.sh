fd9281086ffa9b1b30cb8146d8727d6f32324534
Wolfrax <mats.melander@gmail.com>
2015-07-16 10:09:41 +0200

    Update Swind.sh

--- Swind.sh
+++ Swind.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # See https://help.github.com/articles/generating-ssh-keys/ to enable SSH to GitHub, needed for sript
+echo ---------------------
 date
 cd /var/www/Swind
 python /var/www/Swind/py/getSMHI.py
