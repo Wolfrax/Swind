cc366c9f466d9cffb58383489e954d1aecc51bd3
Wolfrax <mats.melander@gmail.com>
2015-07-16 09:39:00 +0200

    Make script work for crontab

--- Swind.sh
+++ Swind.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # See https://help.github.com/articles/generating-ssh-keys/ to enable SSH to GitHub, needed for sript
+date
+cd /var/www/Swind
 python /var/www/Swind/py/getSMHI.py
 git add ./data
 git commit -q -m 'Latest day'
