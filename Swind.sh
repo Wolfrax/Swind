9fbd48f7303a7dba06cfd9774d39bf3b195af802
Mats Melander <mats.melander@gmail.com>
2015-08-06 22:15:12 +0200

    Cleanup

--- Swind.sh
+++ Swind.sh
@@ -8,15 +8,15 @@ python /var/www/Swind/py/getSMHI.py
 git pull
 git add data            # Add all new files in data-folder
 git rm -f data/OLD_*.js # Remove all files marked with "OLD_"-prefix by getSMHI.py
-git commit -a -q -m 'Latest day'
-git push -q origin master
+git commit -a -m 'Latest day'
+git push origin master
 echo Update Wolfrax web
 cd /var/www/Wolfrax
 git pull
 git rm -f /var/www/Wolfrax/Swind/data/*.js
 cp -p /var/www/Swind/data/*.js /var/www/Wolfrax/Swind/data
 git add Swind/data/
-git commit -a -q -m 'Latest day'
-git push -q origin master
+git commit -a -m 'Latest day'
+git push origin master
 echo =====================
 echo
