197b30eaddb1326b03ddadf87acaa1120f0a6b66
Mats Melander <mats.melander@gmail.com>
2015-12-03 19:22:48 +0100

    Update shell script

--- Swind.sh
+++ Swind.sh
@@ -3,18 +3,17 @@
 echo ---------------------
 date
 echo Update Swind
-cd /var/www/Swind
 git pull
-python /var/www/Swind/py/getSMHI.py
+python py/getSMHI.py
 git add data            # Add all new files in data-folder
 git rm -f data/OLD_*.js # Remove all files marked with "OLD_"-prefix by getSMHI.py
 git commit -a -m 'Latest day'
 git push origin master
 echo Update Wolfrax web
-cd /var/www/Wolfrax
+cd ../wolfrax.github.io
 git pull
-git rm -f /var/www/Wolfrax/Swind/data/*.js
-cp -p /var/www/Swind/data/*.js /var/www/Wolfrax/Swind/data
+git rm -f Swind/data/*.js
+cp -p ../Swind/data/*.js Swind/data
 git add Swind/data/
 git commit -a -m 'Latest day'
 git push origin master
